2ff083f3dd1341cc09ec82c0ab93673404be6eb1
vvhob <h2michalski@gmail.com>
2024-04-07 23:23:02 +0200

    user

--- zadanie/LegacyApp/User.cs
+++ zadanie/LegacyApp/User.cs
@@ -4,12 +4,68 @@ namespace LegacyApp
 {
     public class User
     {
-        public object Client { get; internal set; }
-        public DateTime DateOfBirth { get; internal set; }
-        public string EmailAddress { get; internal set; }
-        public string FirstName { get; internal set; }
-        public string LastName { get; internal set; }
+        private string _firstName;
+        public string FirstName 
+        { 
+            get => _firstName; 
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("First name cannot be null or whitespace.");
+                }
+                _firstName = value;
+            }
+        }
+
+        private string _lastName;
+        public string LastName 
+        { 
+            get => _lastName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Last name cannot be null or whitespace.");
+                }
+                _lastName = value;
+            } 
+        }
+        
+        private string _email;
+        public string Email 
+        {
+            get => _email;
+            set
+            {
+                if (value.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Email cannot be null or whitespace.");
+                }
+                if (!value.Contains("@") || !value.Contains("."))
+                {
+                    throw new ArgumentException("Invalid email, @ or '.' missing");
+                }
+                _email = value;
+            } 
+        }
+
+        private DateTime _dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            internal set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException("Date of birth cannot be in the future.");
+                }
+                _dateOfBirth = value;
+            }
+        }
+
         public bool HasCreditLimit { get; internal set; }
         public int CreditLimit { get; internal set; }
+        public Client Client { get; internal set; }
     }
 }
