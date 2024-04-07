8e38f23c675a0953cc54d5962d6d0ca471256542
vvhob <h2michalski@gmail.com>
2024-04-07 23:01:57 +0200

    switched from dict to list

--- zadanie/LegacyApp/ClientRepository.cs
+++ zadanie/LegacyApp/ClientRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace LegacyApp
@@ -9,14 +10,14 @@ namespace LegacyApp
         /// <summary>
         /// This collection is used to simulate remote database
         /// </summary>
-        public static readonly Dictionary<int, Client> Database = new Dictionary<int, Client>()
+        public static readonly List<Client> Database = new List<Client>()
         {
-            {1, new Client{ClientId = 1, Name = "Kowalski", Address = "Warszawa, Złota 12", Email = "kowalski@wp.pl", Type = "NormalClient"}},
-            {2, new Client{ClientId = 2, Name = "Malewski", Address = "Warszawa, Koszykowa 86", Email = "malewski@gmail.pl", Type = "VeryImportantClient"}},
-            {3, new Client{ClientId = 3, Name = "Smith", Address = "Warszawa, Kolorowa 22", Email = "smith@gmail.pl", Type = "ImportantClient"}},
-            {4, new Client{ClientId = 4, Name = "Doe", Address = "Warszawa, Koszykowa 32", Email = "doe@gmail.pl", Type = "ImportantClient"}},
-            {5, new Client{ClientId = 5, Name = "Kwiatkowski", Address = "Warszawa, Złota 52", Email = "kwiatkowski@wp.pl", Type = "NormalClient"}},
-            {6, new Client{ClientId = 6, Name = "Andrzejewicz", Address = "Warszawa, Koszykowa 52", Email = "andrzejewicz@wp.pl", Type = "NormalClient"}}
+            new Client{ClientId = 1, Name = "Kowalski", Address = "Warszawa, Złota 12", Email = "kowalski@wp.pl", Type = "NormalClient"},
+            new Client{ClientId = 2, Name = "Malewski", Address = "Warszawa, Koszykowa 86", Email = "malewski@gmail.pl", Type = "VeryImportantClient"},
+            new Client{ClientId = 3, Name = "Smith", Address = "Warszawa, Kolorowa 22", Email = "smith@gmail.pl", Type = "ImportantClient"},
+            new Client{ClientId = 4, Name = "Doe", Address = "Warszawa, Koszykowa 32", Email = "doe@gmail.pl", Type = "ImportantClient"},
+            new Client{ClientId = 5, Name = "Kwiatkowski", Address = "Warszawa, Złota 52", Email = "kwiatkowski@wp.pl", Type = "NormalClient"},
+            new Client{ClientId = 6, Name = "Andrzejewicz", Address = "Warszawa, Koszykowa 52", Email = "andrzejewicz@wp.pl", Type = "NormalClient"}
         };
         
         /// <summary>
@@ -28,8 +29,9 @@ namespace LegacyApp
             int randomWaitTime = new Random().Next(2000);
             Thread.Sleep(randomWaitTime);
 
-            if (Database.ContainsKey(clientId))
-                return Database[clientId];
+            var client = Database.FirstOrDefault(c => c.ClientId == clientId);
+            if (client != null)
+                return client;
 
             throw new ArgumentException($"User with id {clientId} does not exist in database");
         }
