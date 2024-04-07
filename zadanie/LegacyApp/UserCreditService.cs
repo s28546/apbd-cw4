0f83c18d89a04be8feecdc9c77290600815608ce
vvhob <h2michalski@gmail.com>
2024-04-07 23:18:05 +0200

    usercreditservcie

--- zadanie/LegacyApp/UserCreditService.cs
+++ zadanie/LegacyApp/UserCreditService.cs
@@ -4,31 +4,25 @@ using System.Threading;
 
 namespace LegacyApp
 {
-    public class UserCreditService : IDisposable
+    public class UserCreditService
     {
         /// <summary>
         /// Simulating database
         /// </summary>
-        private readonly Dictionary<string, int> _database =
-            new Dictionary<string, int>()
-            {
-                {"Kowalski", 200},
-                {"Malewski", 20000},
-                {"Smith", 10000},
-                {"Doe", 3000},
-                {"Kwiatkowski", 1000}
-            };
-        
-        public void Dispose()
+        private readonly Dictionary<string, int> _database = new Dictionary<string, int>()
         {
-            //Simulating disposing of resources
-        }
+            {"Kowalski", 200},
+            {"Malewski", 20000},
+            {"Smith", 10000},
+            {"Doe", 3000},
+            {"Kwiatkowski", 1000}
+        };
 
         /// <summary>
         /// This method is simulating contact with remote service which is used to get info about someone's credit limit
         /// </summary>
         /// <returns>Client's credit limit</returns>
-        internal int GetCreditLimit(string lastName, DateTime dateOfBirth)
+        internal int GetCreditLimit(string lastName)
         {
             int randomWaitingTime = new Random().Next(3000);
             Thread.Sleep(randomWaitingTime);
